a96c63bcef21200edaf2f9b66edfb07aea9f2047
Ian Cossentino <icossentino@gmail.com>
2021-03-02 13:44:33 -0500

    commit tree 1c3c595e365b7e8fa90c556ce8ea07850153cfeb

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+
+
+
+function mapToNegativize(numArray) {
+  return numArray.map(e => e * -1)
+}
+
+function mapToNoChange(numArray) {
+  return numArray.map(e => e)
+}
+
+
+function mapToDouble(numArray) {
+  return numArray.map(e => e * 2)
+}
+
+function mapToSquare(numArray) {
+  return numArray.map(e => e ** 2)
+}
+
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  return sourceArray.reduce(reducer, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray, startingPoint=true) {
+  const reducer = function(accumulator, currentValue) {
+    accumulator = !!(accumulator && currentValue)
+    return accumulator
+  }
+  return sourceArray.reduce(reducer, startingPoint)
+}
+
+
+function reduceToAnyTrue(sourceArray, startingPoint=false) {
+  const reducer = function(accumulator, currentValue) {
+    accumulator = !!(accumulator || currentValue)
+    return accumulator
+  }
+  return sourceArray.reduce(reducer, startingPoint)
+}
